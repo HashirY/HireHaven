b301a34aeb7260246d250842493ca43d7d95a281
Hashir <hashir0601.be21@chitkara.edu.in>
2024-02-15 20:39:23 +0530

    added backend link

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log("Db will connect soon1");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://root:root@hirehaven.5z3xxwm.mongodb.net/hirehaven?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected To DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
